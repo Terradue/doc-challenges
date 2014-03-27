58204a207db9723c26aa7efba0e4399cc069851a
Emmanuel Mathot <emmanuel.mahot@gmail.com>
2014-03-27 12:25:19 +0100

    sphinx-1.0-build

--- check_sphinx.py
+++ check_sphinx.py
@@ -4,12 +4,12 @@ import subprocess
 def test_linkcheck(tmpdir):
     doctrees = tmpdir.join("_build/doctrees")
     htmldir = tmpdir.join("_build/html")
-    subprocess.check_call(["sphinx-build", "-W", "-blinkcheck", "-d",
+    subprocess.check_call(["sphinx-1.0-build", "-W", "-blinkcheck", "-d",
         str(doctrees), ".", str(htmldir)])
 
 
 def test_build_docs(tmpdir):
     doctrees = tmpdir.join("_build/doctrees")
     htmldir = tmpdir.join("_build/html")
-    subprocess.check_call([ "sphinx-build", "-n", "-W", "-bhtml", "-d",
+    subprocess.check_call([ "sphinx-1.0-build", "-n", "-W", "-bhtml", "-d",
         str(doctrees), ".", str(htmldir)])
