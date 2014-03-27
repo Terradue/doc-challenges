63c6448a9bdbd72f89b0f4bbce2738b36014c076
Emmanuel Mathot <emmanuel.mathot@skynet.be>
2014-03-27 16:16:50 +0100

    Update check_sphinx.py

--- check_sphinx.py
+++ check_sphinx.py
@@ -5,11 +5,11 @@ def test_linkcheck(tmpdir):
     doctrees = tmpdir.join("_build/doctrees")
     htmldir = tmpdir.join("_build/html")
     subprocess.check_call(["sphinx-1.0-build", "-W", "-blinkcheck", "-d",
-        str(doctrees), ".", str(htmldir)])
+        str(doctrees), "source", str(htmldir)])
 
 
 def test_build_docs(tmpdir):
     doctrees = tmpdir.join("_build/doctrees")
     htmldir = tmpdir.join("_build/html")
     subprocess.check_call([ "sphinx-1.0-build", "-n", "-W", "-bhtml", "-d",
-        str(doctrees), ".", str(htmldir)])
+        str(doctrees), "source", str(htmldir)])
